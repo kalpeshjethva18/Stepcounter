7f64749f501b6ca53a7971aa0f0c9f8271cf0147
kalpeshjethva <macpc@macpc1-2.local>
2016-03-18 10:30:40 +0530

    Initial Commit

---
+++ Stepcounter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stepcounter
+//
+//  Created by macpc on 18/03/16.
+//  Copyright (c) 2016 macpc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
